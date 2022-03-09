3f4ad6b63b60e98c680db9a1ebdcfe112e833ecc
patgit-design <patricia.corduant@gmail.com>
2022-03-09 09:01:00 +0100

    stylizing

--- index.php
+++ index.php
@@ -25,23 +25,6 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Nunito:wght@700&display=swap" rel="stylesheet">
-        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/manifest.json">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-        <meta name="theme-color" content="#ffffff">
 
 
     </head>
@@ -53,8 +36,8 @@
     <main>
 
         <div class="container-fluid bg-app ">
-            <div class="row text-center">
-                <img class="logo mt-5 pt-5" src="./img/logo-white.png" alt="Logo de Gestion Informatique Professionelle">
+            <div class="row ">
+                <img class="logo mx-auto pt-5" src="./img/logo-white.png" alt="Logo de Gestion Informatique Professionelle">
             </div>
             <div class="col-sm-8 col-md-8 bg-survey ">
                 <div class="container-fluid mb-5">
